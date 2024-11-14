51f394f1577691e785aaa956562cc4300e1e301c
ninja-NikhilChauhan <87414959+content-cn@users.noreply.github.com>
2024-11-14 15:03:52 +0530

    Week-2-class-2 some changes in route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 // routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
 
 // Define routes
