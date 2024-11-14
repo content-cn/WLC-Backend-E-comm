10228f42f6a4e054970994d21b30898409857885
ninja-NikhilChauhan <87414959+content-cn@users.noreply.github.com>
2024-11-14 13:36:27 +0530

    Week-2-class-2

--- server.js
+++ server.js
@@ -52,5 +52,6 @@ app.use('/productlists', productRoutes);
 // Define the port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log("Server is running on http://localhost:${PORT}");
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
+
